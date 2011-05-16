002bd24d7b1aca1d09f527c549460f6d25b353bd
lee <lee@lee-laptop.(none)>
2011-05-16 23:27:30 +0800

    add master

---
+++ public/javascripts/extend/ui.js
@@ -0,0 +1,18 @@
+function selectShare(share) {
+    $('.share-type').hide();
+    $('#'+share).show();
+    $('#'+share+' :input[type!="hidden"]')[0].focus();
+    $('.share-selector a').removeClass('active');
+    $('#'+share+'-icon').addClass('active');
+}
+function showShare() {
+    $('.share-details').show();
+    $('#share-starter').hide();
+    var first = $('.share .share-type:first').attr('id');
+    selectShare(first);
+}
+function hideShare() {
+    $('.share-type, .share-details').hide();
+    $('#share-starter').show();
+}
+$('#share-starter').live('focus', showShare);
