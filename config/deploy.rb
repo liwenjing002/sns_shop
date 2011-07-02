1889e10a13f468f91799b57e827d3577854cfbe0
lee <lee@lee-laptop.(none)>
2011-07-02 13:17:40 +0800

    2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,10 @@
 require File.expand_path("../../lib/rvm_cap", __FILE__)
 
-set :rvm_ruby_string, 'ree@onebody'
-#set :rvm_bin_path, "$HOME/bin"
+set :rvm_ruby_string, '1.8.7@onebody'
+set :rvm_bin_path, "$HOME/bin"
 # point to your server
-set :host, '127.0.0.1'
+#set :host, '127.0.0.1'
+set :host,'50.19.219.48'
 ssh_options[:keys] = ["#{ENV['HOME']}/sns2.pem"]
 # if you have multiple servers, point these individually
 role :web, host
@@ -14,7 +15,8 @@ set :repository, "git://github.com/0612800232/sns_shop.git"
 set :scm, :git
 
 set :application, 'onebody'
-set :user, 'lee'
+#set :user, 'lee'
+set :user,'ubuntu'
 set :runner, user
 set :repository_cache, 'git_cache'
 set :deploy_via, :remote_cache
